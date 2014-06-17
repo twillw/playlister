5f13d1191890446868a1e0ac337a5c870bdca289
twillw <tom@tomwark.me>
2014-06-16 21:17:55 -0400

    youtube player loading video

--- app/scripts/controllers/player.js
+++ app/scripts/controllers/player.js
@@ -1,9 +1,26 @@
 'use strict';
 
 angular.module('playlisterApp')
-.controller('PlayerCtrl', function ($scope, $http, $location) {
+.controller('PlayerCtrl', function ($scope, $window) {
     var params = { allowScriptAccess: "always" };
-    var atts = { id: "myytplayer" };
+    var atts = { id: "ytSongPlayer" };
+    $scope.currentPlayer = {};
     swfobject.embedSWF("http://www.youtube.com/apiplayer?enablejsapi=1&version=3",
                        "ytSongPlayer", "355", "200", "8", null, null, params, atts);
+    $scope.initPlayer = function (currentSong) {
+      var songs  = $scope.currentPlaylist.songs;
+      console.log(songs[currentSong]['songUrl']);
+      console.log($scope.videoEl);
+      $scope.videoEl.loadVideoByUrl(songs[currentSong]['songUrl']);
+    };
+    $window.onYouTubePlayerReady = function() {
+      $scope.videoEl = document.getElementById('ytSongPlayer');
+      $scope.initPlayer(0);
+    };
+    $scope.currentPlayer.play = function () {
+      $scope.videoEl.playVideo();
+    };
+    $scope.currentPlayer.pause = function () {
+      $scope.videoEl.pauseVideo();
+    };
   });
